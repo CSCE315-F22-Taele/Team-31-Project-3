7a505981123c82cf1727eb6a8f0660c176926eeb
gabrieldiaziv <gabrieldiaziv@gmail.com>
2022-11-09 10:14:15 -0600

    styles bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import { type AppType } from "next/app";
+import Head from "next/head";
 
 import { trpc } from "../common/utils/trpc";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "../common/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<link
+					href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+					rel="stylesheet"
+					integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+					crossOrigin="anonymous"
+				/>
+			</Head>
+
+			<Component {...pageProps} />;
+		</>
+	)
 };
 
 export default trpc.withTRPC(MyApp);
