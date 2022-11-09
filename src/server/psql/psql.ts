55ce3c8c76588bcd641abf3d2e9ddb133525e4f9
gabrieldiaziv <gabrieldiaziv@gmail.com>
2022-11-09 09:39:02 -0600

    adding ssl for psql

--- src/server/psql/psql.ts
+++ src/server/psql/psql.ts
@@ -5,7 +5,8 @@ const psql = new Pool({
 	host: process.env.POSTGRES_HOST,
 	database: process.env.POSTGRES_DBNAME,
 	password: process.env.POSTGRES_PASSWORD,
-	port: Number(process.env.POSTGRES_PORT)
+	port: Number(process.env.POSTGRES_PORT),
+	ssl: { rejectUnauthorized: false }
 });
 
 export type DB = Pool;
