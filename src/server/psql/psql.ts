84c709be6a153b9b46e512362b674cb5ec5a3f3d
gabrieldiaziv <gabrieldiaziv@gmail.com>
2022-11-09 14:43:45 -0600

    first commit

--- src/server/psql/psql.ts
+++ src/server/psql/psql.ts
@@ -1,13 +1,21 @@
 import { Pool } from 'pg';
 
-const psql = new Pool({
-	user: process.env.POSTGRES_USER,
-	host: process.env.POSTGRES_HOST,
-	database: process.env.POSTGRES_DBNAME,
-	password: process.env.POSTGRES_PASSWORD,
-	port: Number(process.env.POSTGRES_PORT),
-	ssl: { rejectUnauthorized: false }
-});
+const psqlProps () => {
+	if (Boolean(process.env.DB_DEV) === true)
+		return {
+			user: process.env.POSTGRES_USER,
+			host: process.env.POSTGRES_HOST,
+			database: process.env.POSTGRES_DBNAME,
+			password: process.env.POSTGRES_PASSWORD,
+			port: Number(process.env.POSTGRES_PORT),
+		}
+	const psql = new Pool({
+		user: process.env.POSTGRES_USER,
+		host: process.env.POSTGRES_HOST,
+		database: process.env.POSTGRES_DBNAME,
+		password: process.env.POSTGRES_PASSWORD,
+		port: Number(process.env.POSTGRES_PORT),
+	});
 
-export type DB = Pool;
-export default psql;
+	export type DB = Pool;
+	export default psql;
