9da966b0b4ec3267c9f51a9ef95ba82f2609688a
Jacob Yin <jacoby0729@gmail.com>
2022-11-08 19:23:24 -0600

    More Queries and Report Queries

--- src/server/trpc/router/manager.ts
+++ src/server/trpc/router/manager.ts
@@ -1,127 +1,107 @@
 import { z } from "zod";
 import psql from "../../psql/psql";
-import { InsertInventoryItem, InsertMenuItem, UpdateInvetoryItem, UpdateMenuItem } from "../../psql/queries";
+import * as queries from "../../psql/queries";
+import * as report from "../../psql/reports";
 
 import { router, publicProcedure } from "../trpc";
 
 export const managerRouter = router({
-	sales: publicProcedure
-		.input(z.object({ startDate: z.string(), endDate: z.string() }))
-		.query(({ input }) => {
-			return {
-				salesReport: getSales(),
-			};
-		}),
-	excess: publicProcedure
-		.input(z.object({ startDate: z.string(), endDate: z.string() }))
-		.query(({ input }) => {
-			return {
-				salesReport: getSales(),
-			};
-		}),
+    sales: publicProcedure
+        .input(z.object({ startDate: z.date(), endDate: z.date() }))
+        .query(({ input }) => {
+            return {
+                salesReport: report.salesReport(psql, input.startDate, input.endDate),
+            };
+        }),
+    excess: publicProcedure
+        .input(z.object({ startDate: z.date(), endDate: z.date() }))
+        .query(({ input }) => {
+            return {
+                excessReport: report.excessReport(psql, input.startDate, input.endDate),
+            };
+        }),
+    restock: publicProcedure
+        .query(() => {
+            return {
+                restockReport: report.restockReport(psql),
+            };
+        }),
+    pairs: publicProcedure
+        .input(z.object({ startDate: z.date(), endDate: z.date() }))
+        .query(({ input }) => {
+            return {
+                pairsReport: report.pairsReport(psql, input.startDate, input.endDate),
+            };
+        }),
 
-	updateMenuItem: publicProcedure
-		.input(z.object({
-			menuItemID: z.number(),
-			name: z.string(),
-			description: z.string(),
-			price: z.number(),
-			isEntree: z.boolean(),
-			imageURL: z.string()
-		}))
-		.mutation(async ({ input }) => {
-			await UpdateMenuItem(psql, input);
-		}),
+    updateMenuItem: publicProcedure
+        .input(z.object({
+            menuItemID: z.number(),
+            name: z.string(),
+            description: z.string(),
+            price: z.number(),
+            isEntree: z.boolean(),
+            imageURL: z.string()
+        }))
+        .mutation(async ({ input }) => {
+            await queries.UpdateMenuItem(psql, input);
+        }),
 
-	insertMenuItem: publicProcedure
-		.input(z.object({
-			metaData: z.object({
-				name: z.string(),
-				description: z.string(),
-				price: z.number(),
-				isEntree: z.boolean(),
-				imageURL: z.string()
-			}),
-			ings: z.object({
-				itemID: z.number(),
-				amount: z.number()
-			}).array()
-		}))
-		.mutation(async ({ input }) => {
-			await InsertMenuItem(psql, input.metaData, input.ings);
-		}),
+    insertMenuItem: publicProcedure
+        .input(z.object({
+            metaData: z.object({
+                name: z.string(),
+                description: z.string(),
+                price: z.number(),
+                isEntree: z.boolean(),
+                imageURL: z.string()
+            }),
+            ings: z.object({
+                itemID: z.number(),
+                amount: z.number()
+            }).array()
+        }))
+        .mutation(async ({ input }) => {
+            await queries.InsertMenuItem(psql, input.metaData, input.ings);
+        }),
 
-	insertInventoryItem: publicProcedure
-		.input(z.object({
-			name: z.string(),
-			unitPrice: z.number(),
-			expirationDate: z.date(),
-			stock: z.number(),
-			restockThreshold: z.number()
-		}))
-		.mutation(async ({ input }) => {
-			await InsertInventoryItem(psql, input)
-		}),
+    insertInventoryItem: publicProcedure
+        .input(z.object({
+            name: z.string(),
+            unitPrice: z.number(),
+            expirationDate: z.date(),
+            stock: z.number(),
+            restockThreshold: z.number()
+        }))
+        .mutation(async ({ input }) => {
+            await queries.InsertInventoryItem(psql, input)
+        }),
 
-	updateInventoryItem: publicProcedure
-		.input(z.object({
-			itemID: z.number(),
-			name: z.string(),
-			unitPrice: z.number(),
-			expirationDate: z.date(),
-			stock: z.number(),
-			restockThreshold: z.number()
-		}))
-		.mutation(async ({ input }) => {
-			await UpdateInvetoryItem(psql, input)
-		})
+    updateInventoryItem: publicProcedure
+        .input(z.object({
+            itemID: z.number(),
+            name: z.string(),
+            unitPrice: z.number(),
+            expirationDate: z.date(),
+            stock: z.number(),
+            restockThreshold: z.number()
+        }))
+        .mutation(async ({ input }) => {
+            await queries.UpdateInvetoryItem(psql, input)
+        }),
+    menuItems: publicProcedure
+        .query(() => {
+            return {
+                menuitems: queries.selectMenuItems(psql),
+            }
+
+        }),
+    inventoryItems: publicProcedure
+        .query(() => {
+            return {
+                menuitems: queries.selectInventoryItems(psql),
+            }
+
+        }),
 });
 
-interface saleItem {
-	name: string,
-	sales: number,
-}
-
-function getSales(): saleItem[] {
-	return []
-}
-
-interface excessItem {
-	itemId: number,
-	name: string,
-	startDate: Date,
-	endDate: Date,
-	itemsUsed: number,
-	startStock: number,
-	endStock: number,
-
-}
-
-function getExcess(): excessItem[] {
-	return []
-}
-
-interface pairItem {
-	itemID1: number,
-	itemID2: number,
-	itemName1: string,
-	itemName2: string,
-	amount: number,
-
-}
-
-function getPairs(): pairItem[] {
-	return []
-}
-
-interface restockItem {
-	itemID: number,
-	itemName: string,
-	price: number,
-	stock: number,
-
-}
-
-function getRestock(): restockItem[] {
-	return []
-}
