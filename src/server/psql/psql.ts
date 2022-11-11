9a046281c33f0a08d05baf0e509746688c370ee0
gabrieldiaziv <gabrieldiaziv@gmail.com>
2022-11-11 16:34:56 -0600

    added detection for which db to use

--- src/server/psql/psql.ts
+++ src/server/psql/psql.ts
@@ -1,21 +1,22 @@
 import { Pool } from 'pg';
 
-const psqlProps () => {
-	if (Boolean(process.env.DB_DEV) === true)
-		return {
-			user: process.env.POSTGRES_USER,
-			host: process.env.POSTGRES_HOST,
-			database: process.env.POSTGRES_DBNAME,
-			password: process.env.POSTGRES_PASSWORD,
-			port: Number(process.env.POSTGRES_PORT),
-		}
-	const psql = new Pool({
+function init() {
+
+	const props: any = {
 		user: process.env.POSTGRES_USER,
 		host: process.env.POSTGRES_HOST,
 		database: process.env.POSTGRES_DBNAME,
 		password: process.env.POSTGRES_PASSWORD,
 		port: Number(process.env.POSTGRES_PORT),
-	});
+	};
 
-	export type DB = Pool;
-	export default psql;
+	if (!Boolean(process.env.DEV_DB))
+		props.ssl = { rejectUnauthorized: false }
+
+	return new Pool(props);
+}
+
+
+const psql = init();
+export type DB = Pool;
+export default psql;
