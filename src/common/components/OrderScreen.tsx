605ac6120ee08da11ae39decc1fd5813b15d200f
gabrieldiaziv <gabrieldiaziv@gmail.com>
2022-12-06 03:08:18 -0600

    server cart styling

--- src/common/components/OrderScreen.tsx
+++ src/common/components/OrderScreen.tsx
@@ -19,198 +19,198 @@ import { MenuOrder } from "../interfaces/client";
 import { useSession } from "next-auth/react";
 
 type OrderProps = {
-	orderItems: MenuOrder[],
-	setOrderItems: React.Dispatch<React.SetStateAction<MenuOrder[]>>,
-	showImages: boolean
+  orderItems: MenuOrder[],
+  setOrderItems: React.Dispatch<React.SetStateAction<MenuOrder[]>>,
+  showImages: boolean
 };
 function OrderScreen({ orderItems, setOrderItems, showImages }: OrderProps) {
-	const menu = trpc.orders.menuItems.useQuery();
-	if (!menu.data)
-		return <div>loading...</div>;
-	const { entrees, sides } = menu.data.menuItems;
-	return (
-		<>
-			<Tabs
-				defaultActiveKey="entrees"
-				className="mb-3"
-			>
-				<Tab eventKey="entrees" title="Entrees">
-					<Tabs
-						defaultActiveKey="burgers"
-						className="mb-3"
-					>
-						<Tab eventKey="burgers" title="Burgers">
-							<div className={styles.cardGrid}>
-								{entrees.burger.map((e: MenuItem) => {
-									return (
-										<ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
-									)
-								})
-								}
-							</div>
-						</Tab>
-						<Tab eventKey="chicken" title="Chicken">
-							<div className={styles.cardGrid}>
-								{entrees.chicken.map((e: MenuItem) => {
-									return (
-										<ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
-									)
-								})
-								}
-							</div>
-						</Tab>
-						<Tab eventKey="salad" title="Salads">
-							<div className={styles.cardGrid}>
-								{entrees.salad.map((e: MenuItem) => {
-									return (
-										<ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
-									)
-								})
-								}
-							</div>
-						</Tab>
-					</Tabs>
-				</Tab>
-				<Tab eventKey="sides" title="Sides">
-					<Tabs
-						defaultActiveKey="fried"
-						className="mb-3"
-					>
-						<Tab eventKey="fried" title="Savory">
-							<div className={styles.cardGrid}>
-								{sides.dessert.map((e: MenuItem) => {
-									return (
-										<ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
-									)
-								})
-								}
-							</div>
-						</Tab>
-						<Tab eventKey="dessert" title="Desserts">
-							<div className={styles.cardGrid}>
-								{sides.fried.map((e: MenuItem) => {
-									return (
-										<ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
-									)
-								})
-								}
-							</div>
-						</Tab>
-						<Tab eventKey="drinks" title="Drinks">
-							<div className={styles.cardGrid}>
-								{sides.drink.map((e: MenuItem) => {
-									return (
-										<ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
-									)
-								})
-								}
-							</div>
-						</Tab>
-					</Tabs>
-				</Tab>
-			</Tabs>
-		</>
-	)
+  const menu = trpc.orders.menuItems.useQuery();
+  if (!menu.data)
+    return <div>loading...</div>;
+  const { entrees, sides } = menu.data.menuItems;
+  return (
+    <>
+      <Tabs
+        defaultActiveKey="entrees"
+        className="mb-3"
+      >
+        <Tab eventKey="entrees" title="Entrees">
+          <Tabs
+            defaultActiveKey="burgers"
+            className="mb-3"
+          >
+            <Tab eventKey="burgers" title="Burgers">
+              <div className={styles.cardGrid}>
+                {entrees.burger.map((e: MenuItem) => {
+                  return (
+                    <ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
+                  )
+                })
+                }
+              </div>
+            </Tab>
+            <Tab eventKey="chicken" title="Chicken">
+              <div className={styles.cardGrid}>
+                {entrees.chicken.map((e: MenuItem) => {
+                  return (
+                    <ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
+                  )
+                })
+                }
+              </div>
+            </Tab>
+            <Tab eventKey="salad" title="Salads">
+              <div className={styles.cardGrid}>
+                {entrees.salad.map((e: MenuItem) => {
+                  return (
+                    <ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
+                  )
+                })
+                }
+              </div>
+            </Tab>
+          </Tabs>
+        </Tab>
+        <Tab eventKey="sides" title="Sides">
+          <Tabs
+            defaultActiveKey="fried"
+            className="mb-3"
+          >
+            <Tab eventKey="fried" title="Savory">
+              <div className={styles.cardGrid}>
+                {sides.dessert.map((e: MenuItem) => {
+                  return (
+                    <ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
+                  )
+                })
+                }
+              </div>
+            </Tab>
+            <Tab eventKey="dessert" title="Desserts">
+              <div className={styles.cardGrid}>
+                {sides.fried.map((e: MenuItem) => {
+                  return (
+                    <ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
+                  )
+                })
+                }
+              </div>
+            </Tab>
+            <Tab eventKey="drinks" title="Drinks">
+              <div className={styles.cardGrid}>
+                {sides.drink.map((e: MenuItem) => {
+                  return (
+                    <ItemCard key={e.menuItemID} showImages={showImages} menuItem={e} orderItems={orderItems} setOrderItems={setOrderItems} />
+                  )
+                })
+                }
+              </div>
+            </Tab>
+          </Tabs>
+        </Tab>
+      </Tabs>
+    </>
+  )
 }
 
 export default OrderScreen;
 
 
 type ItemCardProps = {
-	menuItem: MenuItem,
-	orderItems: MenuOrder[],
-	setOrderItems: React.Dispatch<React.SetStateAction<MenuOrder[]>>,
-	showImages: boolean,
+  menuItem: MenuItem,
+  orderItems: MenuOrder[],
+  setOrderItems: React.Dispatch<React.SetStateAction<MenuOrder[]>>,
+  showImages: boolean,
 };
 
-const ItemCard = ({
-	menuItem,
-	orderItems,
-	setOrderItems,
-	showImages,
+export const ItemCard = ({
+  menuItem,
+  orderItems,
+  setOrderItems,
+  showImages,
 }: ItemCardProps) => {
-	const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-	const handleClose = () => setShow(false);
-	const [ingsUsed, setIngsUsed] = useState<{ itemID: number, name: string}[]>([]);
-	const handleShow = () => setShow(true);
-	const addOrderItem = (menuItem: MenuItem, notes: string) => {
-		const item = orderItems.find(orderItem => orderItem.menuItemID == menuItem.menuItemID && orderItem.ingsUsed.every(ing => ingsUsed.includes(ing)));
-		if (!menuItem.menuItemID)
-			return;
-		if (!item)
-			orderItems.push({
-				menuItemID: menuItem!.menuItemID,
-				menuItemName: menuItem.name,
-				price: menuItem.price,
-				amount: 1,
-				ingsUsed: ingsUsed,
-				notes: notes,
-			});
-		else
-			item.amount += 1;
-		setOrderItems([...orderItems]);
-		setIngsUsed([]);
-		console.log(orderItems);
-		setShow(false);
-	};
+  const handleClose = () => setShow(false);
+  const [ingsUsed, setIngsUsed] = useState<{ itemID: number, name: string }[]>([]);
+  const handleShow = () => setShow(true);
+  const addOrderItem = (menuItem: MenuItem, notes: string) => {
+    const item = orderItems.find(orderItem => orderItem.menuItemID == menuItem.menuItemID && orderItem.ingsUsed.every(ing => ingsUsed.includes(ing)));
+    if (!menuItem.menuItemID)
+      return;
+    if (!item)
+      orderItems.push({
+        menuItemID: menuItem!.menuItemID,
+        menuItemName: menuItem.name,
+        price: menuItem.price,
+        amount: 1,
+        ingsUsed: ingsUsed,
+        notes: notes,
+      });
+    else
+      item.amount += 1;
+    setOrderItems([...orderItems]);
+    setIngsUsed([]);
+    console.log(orderItems);
+    setShow(false);
+  };
 
-	if (!menuItem.menuItemID)
-		return <div>error</div>
+  if (!menuItem.menuItemID)
+    return <div>error</div>
 
-	const ingsData = trpc.orders.ings.useQuery({ menuItemID: menuItem.menuItemID });
+  const ingsData = trpc.orders.ings.useQuery({ menuItemID: menuItem.menuItemID });
 
-	if (!ingsData || !ingsData.data || !ingsData.data.ings)
-		return (<div>loading...</div>)
+  if (!ingsData || !ingsData.data || !ingsData.data.ings)
+    return (<div>loading...</div>)
 
-	// useEffect(() => {
-	// 	setIngsUsed(ingsData.data.ings)
-	// }, [ingsData.data])
+  // useEffect(() => {
+  // 	setIngsUsed(ingsData.data.ings)
+  // }, [ingsData.data])
 
-	const addOrRemove = (ingsID: number, name: string) => {
-		
-		console.log(ingsUsed);
-		if (ingsUsed.find(id => id.itemID === ingsID)) {
-			const updatedIngs = ingsUsed.filter((id) => id.itemID !== ingsID)
-			setIngsUsed([...updatedIngs]);
-			return
-		}
+  const addOrRemove = (ingsID: number, name: string) => {
 
-		setIngsUsed([...ingsUsed, {itemID: ingsID, name: name}]);
+    console.log(ingsUsed);
+    if (ingsUsed.find(id => id.itemID === ingsID)) {
+      const updatedIngs = ingsUsed.filter((id) => id.itemID !== ingsID)
+      setIngsUsed([...updatedIngs]);
+      return
+    }
 
-	}
+    setIngsUsed([...ingsUsed, { itemID: ingsID, name: name }]);
+
+  }
 
 
-	
 
-	return (
-		<>
-			<section onClick={handleShow} className={styles.card}>
-			<h2 className={styles.cardTitle}>{menuItem.name}</h2>
-			{showImages && <img src={menuItem.imageURL} />}
-			<p className={styles.cardDescription}>${menuItem.price}</p>
-			{/* <Button className="custom-btn" variant="primary" onClick={handleShow}>Add to Cart</Button> */}
-			</section>
-			<Modal show={show} onHide={handleClose}>
-				<Modal.Header closeButton>
-					<Modal.Title>Add Item to Cart?</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<Form>
-					{ingsData.data.ings && ingsData.data.ings.map((ing) => {
-						return <Form.Check key={ing.itemID} defaultChecked={true} label={ing.name} onClick={(e) => addOrRemove(ing.itemID, ing.name)}/>
-					})}
-					</Form>
-				</Modal.Body>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={handleClose}>
-						Close
-					</Button>
-					<Button variant="primary" onClick={() => addOrderItem(menuItem, '')}>
-						Confirm
-					</Button>
-				</Modal.Footer>
-			</Modal>
-			</>
-	);
+
+  return (
+    <>
+      <section onClick={handleShow} className={styles.card}>
+        <h2 className={styles.cardTitle}>{menuItem.name}</h2>
+        {showImages && <img src={menuItem.imageURL} />}
+        <p className={styles.cardDescription}>${menuItem.price}</p>
+        {/* <Button className="custom-btn" variant="primary" onClick={handleShow}>Add to Cart</Button> */}
+      </section>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Item to Cart?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            {ingsData.data.ings && ingsData.data.ings.map((ing) => {
+              return <Form.Check key={ing.itemID} defaultChecked={true} label={ing.name} onClick={(e) => addOrRemove(ing.itemID, ing.name)} />
+            })}
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => addOrderItem(menuItem, '')}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
--- src/pages/server.tsx
+++ src/pages/server.tsx
@@ -10,94 +10,94 @@ import Card from 'react-bootstrap/Card';
 
 
 import RevsHeader from "../common/components/RevsHeader"
+import ServerOrderScreen from "../common/components/ServerOrderScreen";
 
 const Menu: NextPage = () => {
 
-	const [show, setShow] = useState(false);
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-	const [orderItems, setOrderItems] = useState<MenuOrder[]>([]);
-	const sum = (): number => {
-		if (orderItems.length === 0)
-			return 0.0;
-		return orderItems.map((o) => o.amount * o.price)
-			.reduce((_sum: number, x) => _sum + x);
-	}
-
-
-	const order = trpc.orders.order.useMutation()
-	const createOrder = async () => {
-		if (orderItems.length === 0)
-			return;
-		const _orderItems: { menuItemID: number, notes: string, ings: number[] }[] = [];
-
-		// TODO: update the way ings are set
-		orderItems.forEach((o: MenuOrder) => {
-			const arr = new Array(o.amount).fill({ menuItemID: o.menuItemID, notes: o.notes, ings: o.ingsUsed.map(ing => ing.itemID) });
-			_orderItems.push(...arr);
-		});
-		setShow(true);
-		await order.mutateAsync({
-			customerName: "bob",
-			employeeID: 12345,
-			orderItems: _orderItems,
-		});
-
-		if (order.error) {
-			console.log(`ORDER FAILDED: ${order.error.message}`);
-			return;
-		}
-
-		handleShow();
-		setOrderItems([]);
-	};
+  const [orderItems, setOrderItems] = useState<MenuOrder[]>([]);
+  const sum = (): number => {
+    if (orderItems.length === 0)
+      return 0.0;
+    return orderItems.map((o) => o.amount * o.price)
+      .reduce((_sum: number, x) => _sum + x);
+  }
 
 
-	return (
-		<>
-			<RevsHeader />
-			<div className="PageWrapper">
-				<h1>
-					Menu
-				</h1>
-				<div className="server-wrapper">
-					<div className="order-items">
-						<Modal show={show} onHide={handleClose}>
-							<Modal.Header closeButton>
-								<Modal.Title id="contained-modal-title-vcenter">
-									ORDER ID: {order.isLoading ? "loading..." : order.data?.orderID}
-								</Modal.Title>
-							</Modal.Header>
-							<Modal.Footer>
-								<Button onClick={handleClose}>Close</Button>
-							</Modal.Footer>
-						</Modal>
-						<OrderScreen orderItems={orderItems} setOrderItems={setOrderItems} showImages={false} />
-					</div>
-					<Card style={{minWidth:'550px'}}>
-					<Card.Header>My Order</Card.Header>
-					<Card.Body>
-					<div className="cart-items">
-						<Cart isServer={true} orderItems={orderItems} setOrderItems={setOrderItems} />
-					
-						</div>
-					</Card.Body>
-					<Card.Body >
-							<div> COST: ${sum().toFixed(2)} </div>
-							<div> TAX: ${(sum() * .07).toFixed(2)} </div>
-							<div> TOTAL: ${(sum() * 1.07).toFixed(2)} </div>
-							<Button onClick={createOrder} disabled={order.isLoading}>ORDER</Button>
-					</Card.Body>
-					{/* <div style={{bottom: '10px', position:'absolute'}}>
-							
-					</div> */}
-					<Card.Footer></Card.Footer>
-					</Card>
-					
-				</div>
-			</div>
-		</>
-	)
+  const order = trpc.orders.order.useMutation()
+  const createOrder = async () => {
+    if (orderItems.length === 0)
+      return;
+    const _orderItems: { menuItemID: number, notes: string, ings: number[] }[] = [];
+
+    // TODO: update the way ings are set
+    orderItems.forEach((o: MenuOrder) => {
+      const arr = new Array(o.amount).fill({ menuItemID: o.menuItemID, notes: o.notes, ings: o.ingsUsed.map(ing => ing.itemID) });
+      _orderItems.push(...arr);
+    });
+    setShow(true);
+    await order.mutateAsync({
+      customerName: "bob",
+      employeeID: 12345,
+      orderItems: _orderItems,
+    });
+
+    if (order.error) {
+      console.log(`ORDER FAILDED: ${order.error.message}`);
+      return;
+    }
+
+    handleShow();
+    setOrderItems([]);
+  };
+
+
+  return (
+    <>
+      <RevsHeader />
+      <div className="PageWrapper">
+        <h1>
+          Menu
+        </h1>
+        <div className="server-wrapper" style={{ height: '100%' }}>
+          <div className="order-items" >
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                  ORDER ID: {order.isLoading ? "loading..." : order.data?.orderID}
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Footer>
+                <Button onClick={handleClose}>Close</Button>
+              </Modal.Footer>
+            </Modal>
+            <ServerOrderScreen orderItems={orderItems} setOrderItems={setOrderItems} showImages={false} />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <Card>
+              <Card.Header>Customer's Order</Card.Header>
+              <Card.Body>
+                <div style={{ display: 'flex', flexDirection: 'column', width: '100%', minWidth: '550px' }}>
+                  <div className="cart-items" style={{ marginBottom: '1rem', height: '80vh', borderRadius: '2%', backgroundColor: '#F7F7F7', overflow: 'auto' }} >
+                    <Cart isServer={true} orderItems={orderItems} setOrderItems={setOrderItems} />
+                  </div>
+                  <Button style={{ alignSelf: 'flex-end', backgroundColor: '#842323', width: '100%', fontSize: '1.5rem' }}
+                    onClick={createOrder}
+                    disabled={order.isLoading}>
+                    Order {sum() !== 0 ?
+                      <>: ${sum().toFixed(2)} ➡️ </>
+                      : <> 🚫</>
+                    } </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 export default Menu;
