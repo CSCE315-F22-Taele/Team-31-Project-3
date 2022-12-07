fa8cfb9996c4d95f10d92af857592d6dd6af4b0e
Jacob Yin <jacoby0729@gmail.com>
2022-12-06 21:39:35 -0600

    Finalized Google Map

--- src/common/components/GoogleMaps.tsx
+++ src/common/components/GoogleMaps.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef } from "react";
-import { GoogleMap, useLoadScript, Marker, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript, Marker, LoadScript, MarkerF } from "@react-google-maps/api";
 import { Loader } from '@googlemaps/js-api-loader';
 import React from 'react'
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: '800px',
+  height: '500px',
+  margin: 'auto'
 };
 
 const center = {
@@ -14,18 +15,28 @@ const center = {
 };
 
 function GoogleMaps() {
+
+
   return (
     <LoadScript
       googleMapsApiKey="AIzaSyAvMFjsWEHwpCVwBTOquMTfnUAoDijQy6g"
+
     >
+
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={15}
+
       >
-        { /* Child components, such as markers, info windows, etc. */}
-        <></>
+        <MarkerF
+          visible={true}
+          position={center}
+        />
+
+
       </GoogleMap>
+
     </LoadScript>
   )
 }
