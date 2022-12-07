bed116c3e59780a7b3d40dfad3ab0c1dae0766eb
Jacob Yin <jacoby0729@gmail.com>
2022-12-06 22:52:03 -0600

    Update map for landing

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -9,31 +9,34 @@ import GoogleMaps from "../common/components/GoogleMaps";
 
 const Landing: NextPage = () => {
 
-	const [show, setShow] = useState(false);
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <>
             <RevsHeader />
             <div className="PageWrapper" id="landingPage">
-                <div style={{display:"flex", justifyContent:"center",}}>
+                <div style={{ display: "flex", justifyContent: "center", }}>
                     <Image alt="Rev's American Grill Logo" src={logo} width={719} height={352} />
                 </div>
-                <div style={{paddingTop:"70px",}}>
+                <div style={{ paddingTop: "70px", }}>
                     <h2 className="landing">
                         One of the best reasons to visit College Station is to experience Rev's American Grill. We offer a great time for people everywhere and our food keeps people coming back for more.
                     </h2>
                 </div>
-                <div style={{paddingTop:"50px", display:"flex", justifyContent:"center",}}>
+                <div style={{ paddingTop: "50px", display: "flex", justifyContent: "center", }}>
                     <Button className="custom-btn-landing" href="/">Order Now</Button>
                 </div>
                 <div className="PageWrapper" id="googleMaps">
-                    <h2 className="mapsHeader">
+                    <h2 className="mapsHeader" style={{ display: "flex", justifyContent: "center", }}>
                         Find Us Here!
                     </h2>
+
+
+
                     <GoogleMaps />
-                </div>     
+                </div>
             </div>
         </>
     )
