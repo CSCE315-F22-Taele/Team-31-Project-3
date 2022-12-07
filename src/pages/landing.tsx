64a730b96c61717b4ede869e34c3fb76ff83fd0c
Minji Choi <mchoi0320@tamu.edu>
2022-12-06 20:36:29 -0600

    updated landing page with logo

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -3,8 +3,9 @@ import { Button } from "react-bootstrap";
 import React, { useState } from "react";
 import { trpc } from "../common/utils/trpc";
 import RevsHeader from "../common/components/RevsHeader";
-import bg from "../common/components/images/revs.png";
-import logo from "../common/components/images/logo.svg";
+import bg from "../common/images/revs.png";
+import Image from "next/image";
+import logo from "../common/images/logo.png";
 
 const Landing: NextPage = () => {
 
@@ -15,9 +16,18 @@ const Landing: NextPage = () => {
     return (
         <>
             <RevsHeader />
-            <div className="PageWrapper" style={{backgroundImage:'url(${bg.src})', width:'100%', height:'100%'}}>
-                <img src={logo} id="landingLogo" alt="Rev's American Grill Logo" />
-                
+            <div className="PageWrapper" style={{backgroundImage:'url(${bg.src})', width:'100%', height:'100%', position:'absolute',}}>
+                <div style={{display:"flex", justifyContent:"center",}}>
+                    <Image src={logo} width={719} height={352} alt="Rev's American Grill Logo" />
+                </div>
+                <div style={{paddingTop:"50px",}}>
+                    <h2>
+                        One of the best reasons to visit College Station is to experience Rev's American Grill. We offer a great time for people everywhere and our food keeps people coming back for more.
+                    </h2>
+                </div>
+                <div style={{paddingTop:"30px", display:"flex", justifyContent:"center",}}>
+                    <Button className="custom-btn-landing" href="/">Order Now</Button>
+                </div>
             </div>
         </>
     )
